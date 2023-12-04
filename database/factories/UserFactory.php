7c8a8d0629e7ef573c453c66c5082c79caed251b
ZombreChristian <christianzombre48@gmail.com>
2023-12-04 18:32:40 +0100

    first commit

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class UserFactory extends Factory
+{
+    protected static ?string $password;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'nom' => $this->faker->lastName,
+            'prenom' => $this->faker->firstName,
+            'sexe' => array_rand(["H", "F"], 1),
+            'email' => $this->faker->unique()->safeEmail(),
+            'telephone1' => $this->faker->unique()->phoneNumber,
+            'telephone2' => $this->faker->unique()->phoneNumber,
+            'pieceIdentite' => array_rand(["CNI", "PASSPORT", "PERMIS DE CONDUIRE"], 1),
+            'numeroPieceIdentite' => $this->faker->unique()->bankAccountNumber,
+            'photo' => $this->faker->imageUrl(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        ];
+    }
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     */
+    public function unverified(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'email_verified_at' => null,
+        ]);
+    }
+}
